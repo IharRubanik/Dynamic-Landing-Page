501a3066fd3165b4b7b934640ac7452e2108cbe0
IharRubanik <74908305+IharRubanik@users.noreply.github.com>
2021-02-23 21:13:38 +0300

    fix: js

--- main.js
+++ main.js
@@ -34,16 +34,20 @@ function landingBaground() {
 
   if ( h > 0 && h < 5 ) {  greeting.textContent = textNight;
     document.body.style.backgroundImage = "url('images/3.jpg')";
+    document.body.style.backgroundSize = "100%";
+
     document.body.style.color = 'white';
      }
      else if  (h >= 5 && h < 11) { greeting.textContent = textMorning;
         document.body.style.backgroundImage = "url('images/1.jpg')";
+        document.body.style.backgroundSize = "100%";
         
 
      }
      else { greeting.textContent = textDay;
         document.body.style.backgroundImage = "url('images/2.jpg')";
         document.body.style.color = 'RosyBrown';
+        document.body.style.backgroundSize = "100%";
     };
  }
  landingBaground();
